1857e18249e66e3bebb3b53a8d6c96e87c8f444a
pungun <tkaltk123@gmail.com>
2019-05-18 18:10:51 +0900

    ball-brick collision

---
+++ BreakBrick/Assets/BallCollision.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extention
+{
+    public static float GetAngle(this Vector3 vec)
+    {
+        return Vector3.Angle(Vector3.right, vec);
+    }
+}
+public class BallCollision : MonoBehaviour
+{
+    private Collision2D RecentCollision=null;
+    private Vector3 Angle=new Vector3(1f,0f);
+    public float Speed=5;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += transform.rotation*Angle * Speed * Time.deltaTime;
+    }
+    private void OnCollision(Collision2D coll)
+    {
+        RecentCollision = coll;
+        var contactPoint = coll.contacts[0].point;
+        var ToContact = new Vector3(contactPoint.x, contactPoint.y) - transform.position;
+        var contactAngle = ToContact.GetAngle();
+        float moveAngle = transform.rotation.eulerAngles.z;
+        var reflectAngle = 90 - moveAngle + contactAngle;
+        moveAngle += reflectAngle * 2;
+        moveAngle %= 360;
+        transform.rotation = Quaternion.Euler(0, 0, moveAngle);
+    }
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        OnCollision(coll);
+    }
+    private void OnCollisionStay2D(Collision2D coll)
+    {
+        OnCollision(coll);
+    }
+}
