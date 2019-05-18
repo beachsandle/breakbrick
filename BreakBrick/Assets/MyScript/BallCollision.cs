86aa31e943b9c1e4b4ff2bf099c72cb610c821ac
irikong <gookirix@gmail.com>
2019-05-18 23:27:59 +0900

    Cleanup and Add Prefabs

--- BreakBrick/Assets/MyScript/BallCollision.cs
+++ BreakBrick/Assets/MyScript/BallCollision.cs
@@ -6,31 +6,31 @@ public static class Extention
 {
     public static float GetAngle(this Vector3 vec)
     {
-        return Vector3.Angle(Vector3.right, vec);
+        return Vector3.Angle(Vector3.up, vec);
     }
 }
 public class BallCollision : MonoBehaviour
 {
-    private Collision2D RecentCollision=null;
-    private Vector3 Angle=new Vector3(1f,0f);
-    public float Speed=5;
-    
+    private Collision2D RecentCollision = null;
+    private Vector3 Angle = new Vector3(0f, 1f);
+    public float Speed = 5;
+
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += transform.rotation*Angle * Speed * Time.deltaTime;
+        transform.position += transform.rotation * Angle * Speed * Time.deltaTime;
     }
     private void OnCollision(Collision2D coll)
     {
         RecentCollision = coll;
         var contactPoint = coll.contacts[0].point;
-        var ToContact = new Vector3(contactPoint.x, contactPoint.y) - transform.position;
+        var ToContact = new Vector3(contactPoint.x, contactPoint.y, 0) - transform.position;
         var contactAngle = ToContact.GetAngle();
         float moveAngle = transform.rotation.eulerAngles.z;
         var reflectAngle = 90 - moveAngle + contactAngle;
