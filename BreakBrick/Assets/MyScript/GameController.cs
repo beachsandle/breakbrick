dfad8d8ed2399576c3493af01bf9c63afe4efb5a
irikong <gookirix@gmail.com>
2019-05-19 11:05:23 +0900

    Add GameController and SpawnBrick

---
+++ BreakBrick/Assets/MyScript/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject brick = null;
+
+    void Start()
+    {
+        SpawnBrick();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnBrick()
+    {
+        for(int x = 0; x < 6; x++)
+        {
+            for(int y = 0; y < 3; y++)
+            {
+                Instantiate(brick, new Vector3(-2.5f + x, 4.5f - y, 0), Quaternion.identity);
+            }
+        }
+    }
+}
+
